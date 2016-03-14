c5fee787df150866bfa46ec1d3d31b06c6685b03
Odin <odinuv@gmail.com>
2016-03-14 17:03:10 +0100

    fix: use explicit CSV format

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import csv
 
+csvlt = '\n'
+csvdel = ','
+csvquo = '"'
+
 with open('in/tables/source.csv', mode='rt', encoding='utf-8') as inFile, open('out/tables/destination.csv', mode='wt', encoding='utf-8') as outFile:
-    writer = csv.DictWriter(outFile, fieldnames = ['number', 'someText', 'doulber_number'], dialect='kbc')
+    writer = csv.DictWriter(outFile, lineterminator=csvlt, delimiter = csvdel, quotechar = csvquo)
     writer.writeheader()
 
     lazyLines = (line.replace('\0', '') for line in inFile)
-    csvReader = csv.DictReader(lazyLines, dialect='kbc')
+    csvReader = csv.DictReader(lazyLines, lineterminator=csvlt, delimiter = csvdel, quotechar = csvquo)
     for row in csvReader:
         # do something and write row
         writer.writerow({'number': row['number'], 'someText': row['someText'], 'double_number': int(row['number']) * 2})
