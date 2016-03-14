eb6d444e3bf8df38b17ef07c2dae30d6c14fe209
Odin <odinuv@gmail.com>
2016-03-14 17:30:27 +0100

    fix: add comments

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import csv
 
+# CSV format settings
 csvlt = '\n'
 csvdel = ','
 csvquo = '"'
 
+# open the input and output files
 with open('in/tables/source.csv', mode='rt', encoding='utf-8') as inFile, open('out/tables/destination.csv', mode='wt', encoding='utf-8') as outFile:
+    # write output file header
     writer = csv.DictWriter(outFile, fieldnames = ['number', 'someText', 'double_number'], lineterminator=csvlt, delimiter = csvdel, quotechar = csvquo)
     writer.writeheader()
 
+	# read input file line-by-line
     lazyLines = (line.replace('\0', '') for line in inFile)
     csvReader = csv.DictReader(lazyLines, lineterminator=csvlt, delimiter = csvdel, quotechar = csvquo)
     for row in csvReader:
