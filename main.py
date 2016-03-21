33f060363ca424d5700301d28aea4f5f1281fa0e
Odin <odinuv@gmail.com>
2016-03-21 11:59:06 +0100

    chore: PEP8 names

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ csvdel = ','
 csvquo = '"'
 
 # open the input and output files
-with open('in/tables/source.csv', mode='rt', encoding='utf-8') as inFile, open('out/tables/destination.csv', mode='wt', encoding='utf-8') as outFile:
+with open('in/tables/source.csv', mode='rt', encoding='utf-8') as in_file, open('out/tables/destination.csv', mode='wt', encoding='utf-8') as out_file:
     # write output file header
-    writer = csv.DictWriter(outFile, fieldnames = ['number', 'someText', 'double_number'], lineterminator=csvlt, delimiter = csvdel, quotechar = csvquo)
+    writer = csv.DictWriter(out_file, fieldnames=['number', 'someText', 'double_number'], lineterminator=csvlt, delimiter=csvdel, quotechar=csvquo)
     writer.writeheader()
 
-	# read input file line-by-line
-    lazyLines = (line.replace('\0', '') for line in inFile)
-    csvReader = csv.DictReader(lazyLines, lineterminator=csvlt, delimiter = csvdel, quotechar = csvquo)
-    for row in csvReader:
+    # read input file line-by-line
+    lazy_lines = (line.replace('\0', '') for line in in_file)
+    csv_reader = csv.DictReader(lazy_lines, lineterminator=csvlt, delimiter=csvdel, quotechar=csvquo)
+    for row in csv_reader:
         # do something and write row
         writer.writerow({'number': row['number'], 'someText': row['someText'], 'double_number': int(row['number']) * 2})
